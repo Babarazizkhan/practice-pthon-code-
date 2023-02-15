9c708fbe1b2d8372895bae809a5d7087b72dd457
Babarazizkhan <109368226+Babarazizkhan@users.noreply.github.com>
2023-02-15 02:02:19 -0330

    upadate the comment
    i can chage the comment section

--- mai.py
+++ mai.py
@@ -1,77 +1,75 @@
 import math
-# print(math.sqrt(16))
+ print(math.sqrt(16))
 
-# print(math.pow(2,5))
-# print(math.pow(2,2))
+ print(math.pow(2,5))
+ print(math.pow(2,2))
 
 
-# print(dir(math))
+print(dir(math))
 
-# import calendar
-# cal = calendar.month[2023,1,2,3]
-# print[cal]
+import calendar
+cal = calendar.month[2023,1,2,3]
+print[cal]
 
-# list(calendar.month_name)
+list(calendar.month_name)
 
-# print(math.log10(10))
+print(math.log10(10))
 
-# print(2-2)
+print(2-2)
 
-# list 
+list 
 
-# expense_list = [12,13,14,15]
-# joe_list = [16,17,18,19]
+expense_list = [12,13,14,15]
+joe_list = [16,17,18,19]
 
-# total = 0
+total = 0
 
-# for item in expense_list:
-#     total = total+item
-# print('total of a:', total)
+for item in expense_list:
+    total = total+item
+print('total of a:', total)
 
-# total = 0 
+total = 0 
 
-# for item in joe_list:
-#     total = total+item
-# print('total of b:', total)
-    # function 
-# def calculate_list(exp):
-#     total = 0
-#     for item in exp:
-#         total = total+item
-#     return total
+for item in joe_list:
+    total = total+item
+print('total of b:', total)
 
-# expense_list = [12,13,14,15]
-# exp_joe_list = [16,17,18,19]
+    function 
+def calculate_list(exp):
+    total = 0
+    for item in exp:
+        total = total+item
+    return total
 
-# expense_total = calculate_list(expense_list)
-# exp_joe_total = calculate_list(exp_joe_list)
+expense_list = [12,13,14,15]
+exp_joe_list = [16,17,18,19]
 
-# print("this is the expense list:", exp_joe_total)
-# print("this is the expense joe list:", exp_joe_total)
+expense_total = calculate_list(expense_list)
+exp_joe_total = calculate_list(exp_joe_list)
+
+print("this is the expense list:", exp_joe_total)
+print("this is the expense joe list:", exp_joe_total)
  
 
 value = {"joe",12 ,"jan",13, "dada",14,"boss",11,"khan",17}
 
-
-
-
-# for  item in values:
-#     print(item)
+for  item in values:
+    print(item)
      
-# print(12 in value)
-# print(19 in value)
-# del value["joe"]
-# print(value.clear())
+print(12 in value)
+print(19 in value)
+del value["joe"]
+print(value.clear())
   
-# print(value)
+print(value)
 
-# exp = [13,13,14,15,16,18,119]
+exp = [13,13,14,15,16,18,119]
 
-# total = 0
+total = 0
 
-# for item in exp:
-#     total = total+item
-# print(f'{total}')
+for item in exp:
+    total = total+item
+print(f'{total}')
 
 def my_function(exp):
    total = 0
